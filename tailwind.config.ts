072d19d2b7ea2da02c7f7d5bfbc71e885de2631d
matheusleite01 <de.leite2014@gmail.com>
2024-09-02 00:57:37 -0300

    feat: add variables and global styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,24 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily:{
+      'Plus': ["Plus Jakarta Sans"]
+    },
     colors: {
       white: "#E5E5E5",
       background: "#000000",
       gray: "#D4D4D4",
-      boxButton:
-        "linear-gradient(90deg, rgba(250,85,96,1) 0%, rgba(231,83,133,1) 45%, rgba(77,145,255,1) 99%);",
+      inputColor: "#27272A",
+      inputBorder: "#6B7280",
+      border: "#424246",
+      erroColor: "#FA5560"
+    },
+    backgroundImage: {
+      "gradient-custom":
+        "linear-gradient(90deg, rgba(250,85,96,1) 0%, rgba(231,83,133,1) 45%, rgba(77,145,255,1) 99%)",
+    },
+    boxShadow: {
+      input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
     },
   },
   plugins: [],
