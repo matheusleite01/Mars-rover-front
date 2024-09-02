d03e72690e955c69d6099a909c6d449454c97291
matheusleite01 <de.leite2014@gmail.com>
2024-09-02 17:17:10 -0300

    feat: chenge api url

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:5000" });
-
+const api = axios.create({
+  baseURL: "https://api-mars-rover-back.onrender.com",
+});
 
 export default api;
