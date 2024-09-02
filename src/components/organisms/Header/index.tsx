83cfb625730de07c6b1b85928f918e5d43883628
matheusleite01 <de.leite2014@gmail.com>
2024-09-02 00:56:10 -0300

    feat: add header component

---
+++ src/components/organisms/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="mt-8 text-center mb-24">
+      <h1 className="text-5xl font-bold">
+        Mars Rover{" "}
+        <span className="bg-gradient-custom h-96 rounded-lg px-1">In Next</span>
+      </h1>
+    </header>
+  );
+};
+export default Header;
